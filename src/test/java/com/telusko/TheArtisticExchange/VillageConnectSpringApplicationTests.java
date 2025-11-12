fc8601976eb6419754fd74e49cf53b41557eb334
Tanushree <115385127+Tanushree055@users.noreply.github.com>
2025-11-12 14:22:22 +0530

    Update VillageConnectSpringApplicationTests.java

--- src/test/java/com/telusko/TheArtisticExchange/VillageConnectSpringApplicationTests.java
+++ src/test/java/com/telusko/TheArtisticExchange/VillageConnectSpringApplicationTests.java
@@ -1,13 +1,1 @@
-package com.telusko.TheArtisticExchange;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VillageConnectSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
